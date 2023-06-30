20425bdc855cec304b6e5cdb6dfe8ec79af17f7b
Bastian F <badmax202@gmail.com>
2023-06-30 09:15:01 +0200

    feat: :hammer: update sample data script

--- sql_scripts/sample_data.sql
+++ sql_scripts/sample_data.sql
@@ -2,25 +2,21 @@
 *  Insertion des données dans les tables de la base de données  *
 **************************************************************  */
 
--- Insertion des données dans la table Role
+-- Insertion des données dans la table Android
 
-INSERT INTO Role VALUES (1, 'ADMIN');
-INSERT INTO Role VALUES (2, 'MODERATOR');
-INSERT INTO Role VALUES (3, 'USER');
+/* Google Pixel 7 3+5 */
+INSERT INTO Android (id, name, updates, security_updates) VALUES (1, 'Android 13', 3, 5);
+/* Asus Zenfone 10  2++4 */
+INSERT INTO Android (id, name, updates, security_updates) VALUES (2, 'Android 13', 2, 4);
 
-/* DEVICE *
-brand, name, announced_date, price, status, height, width, weight, thickness, back,
-display_technology, display_size, ratio, resolution, ram, rom, expandable, 
-soc_name, soc_nm, camera_main, camera_number, ultrawide, telephoto, selfie,
-battery_capacity, charging, wireless, reverse, android, android_updates, security_updates
-*/
+-- Insertion des données dans la table SystemOnChip
+INSERT INTO SystemOnChip (id, name, nm) VALUES (1, 'Google Tensor G2', 5);
+INSERT INTO SystemOnChip (id, name, nm) VALUES (2, 'Qualcomm Snapdragon 8 Gen 2', 4);
 
--- Insertion du Google Pixel 7 dans la table Device
-INSERT INTO Device (brand, name, announced_date, price, status, height, width, weight, thickness, back,
-display_technology, display_size, ratio, resolution, ram, rom, expandable, 
-soc_name, soc_nm, camera_main, camera_number, ultrawide, telephoto, selfie,
-battery_capacity, charging, wireless, reverse, android, android_updates, security_updates)
-VALUES ('Google', 'Pixel 7', '2022-10-06', 649, 'AVAILABLE', 155.6, 73.2, 197, 8.7, 'GLASS',
-'OLED', 6.3, '20:9', '1080x2400', 8, 128, FALSE,
-'Google Tensor G2', 5, 50, 2, TRUE, FALSE, 10.8,
-4355, 20, TRUE, TRUE, 13, 3, 5);
+-- Insertion des données dans la table Memory
+
+INSERT INTO Memory (id, ram, rom, expandable) VALUES (1, 8, 128, FALSE);
+
+-- Insertion de bastianfbr dans la table User
+
+INSERT INTO User (id, username, mail, password, role) VALUES (1, 'bastianfbr', 'bastian@fbr.fr', '1234', 'ADMIN');
