a62e59721e4e14fe6e34a77fa65113e1cd910dbf
Bastian F <badmax202@gmail.com>
2023-07-02 11:17:46 +0200

    feat: :sparkles: add values and load data in file

--- sql_scripts/sample_data.sql
+++ sql_scripts/sample_data.sql
@@ -12,6 +12,8 @@ INSERT INTO Android (id, name, updates, security_updates) VALUES (2, 'Android 13
 -- Insertion des données dans la table SystemOnChip
 INSERT INTO SystemOnChip (id, name, nm) VALUES (1, 'Google Tensor G2', 5);
 INSERT INTO SystemOnChip (id, name, nm) VALUES (2, 'Qualcomm Snapdragon 8 Gen 2', 4);
+INSERT INTO SystemOnChip (id, name, nm) VALUES (3, 'Qualcomm Snapdragon 8+ Gen 1', 4);
+INSERT INTO SystemOnChip (id, name, nm) VALUES (4, 'Qualcomm Snapdragon 695 5G', 6);
 
 -- Insertion des données dans la table Memory
 
@@ -20,3 +22,10 @@ INSERT INTO Memory (id, ram, rom, expandable) VALUES (1, 8, 128, FALSE);
 -- Insertion de bastianfbr dans la table User
 
 INSERT INTO User (id, username, mail, password, role) VALUES (1, 'bastianfbr', 'bastian@fbr.fr', '1234', 'ADMIN');
+
+-- Chargement du fichier CSV dans la table Device
+
+LOAD DATA LOCAL INFILE '../files/device.csv'
+INTO TABLE Device
+FIELDS TERMINATED BY ';'
+IGNORE 1 LINES;
