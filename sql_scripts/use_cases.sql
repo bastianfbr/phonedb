bc9bb7907a266dc5e698b176d546c5c85ea7fafe
Bastian F <badmax202@gmail.com>
2023-07-02 14:01:19 +0200

    fix: :bug: fix request for Pixel use case

--- sql_scripts/use_cases.sql
+++ sql_scripts/use_cases.sql
@@ -17,7 +17,7 @@ WHERE username = 'JoelLeGolem' AND password = 'JESuisUnGol€m';
 INSERT INTO Review (user_id, device_id, design, display, performance, camera, battery, software, final, comment)
 VALUES (( SELECT id FROM User WHERE username = "JoelLeGolem"), ( SELECT id FROM Device WHERE name = 'Samsung Galaxy S22 Ultra'), <design_rating>, <display_rating>, <performance_rating>, <camera_rating>, <battery_rating>, <software_rating>, <final_rating>, 'Petit commentaire.');
 
--- Derya regarde les différences avec le Pixel 7 et le Pixel 7a.
+-- Derya regarde les différences avec le Google Pixel 7 et le Pixel 7a.
 
 SELECT * FROM Device
-WHERE name LIKE 'Pixel 7%' OR name LIKE 'Pixel 7a%';
+WHERE name LIKE 'Google Pixel 7%' OR name LIKE 'Google Pixel 7a%';
