d98341b5035e06dd5e30982dd0befba8fa7681de
Bastian F <badmax202@gmail.com>
2023-07-02 12:33:07 +0200

    fix: 🚨 fix incorrect integer value for boolean warning

--- sql_scripts/sample_data.sql
+++ sql_scripts/sample_data.sql
@@ -39,6 +39,7 @@ SET id = @id, brand = @brand, name = @name, announced_date = STR_TO_DATE(@announ
 price = @price, status = @status, height = @height, width = @width, weight = @weight, 
 thickness = @thickness, back = @back, display_tech = @display_tech, display_size = @display_size, 
 ratio = @ratio, resolution = @resolution, camera_main = @camera_main, camera_number = @camera_number, 
-ultrawide = @ultrawide, telephoto = @telephoto, selfie = @selfie, battery_capacity = @battery_capacity, 
-charging = @charging, wireless = @wireless, reverse = @reverse, memory_id = @memory_id, 
-soc_id = @soc_id, android_id = @android_id; 
+ultrawide = IF(@ultrawide = '1', TRUE, FALSE), telephoto = IF(@telephoto = '1', TRUE, FALSE), 
+selfie = @selfie, battery_capacity = @battery_capacity, charging = @charging, 
+wireless = IF(@wireless = '1', TRUE, FALSE), reverse = IF(@reverse = '1', TRUE, FALSE), 
+memory_id = @memory_id, soc_id = @soc_id, android_id = @android_id; 
